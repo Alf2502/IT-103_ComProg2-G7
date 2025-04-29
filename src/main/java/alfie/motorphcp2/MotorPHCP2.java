701b008d40af125511dc78cfb5d96607dced6a14
Alfie <Alfie@DESKTOP-FGE1IMJ>
2025-04-29 19:02:40 +0800

    IT103 - Com Prog 2 | Alfie

---
+++ src/main/java/alfie/motorphcp2/MotorPHCP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package alfie.motorphcp2;
+
+/**
+ *
+ * @author Alfie
+ */
+public class MotorPHCP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
